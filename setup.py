18a963b673c5ec155c2c6483e9501eed5ec9f90a
Julayi <113028494+JK-JuLaYi@users.noreply.github.com>
2025-10-18 14:30:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="playwright-traceview-html",
-    version="0.1.0",
+    version="0.2.0",
     author="Jaya Krishna",
     author_email="jayakrishna107@gmail.com",
     description="Interactive local HTML report and server for Playwright trace results.",
